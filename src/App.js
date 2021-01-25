15a1d7f75233da72ea59757a2452fe29f632b568
danillog <danillog821@gmail.com>
2021-01-25 18:27:05 -0300

    Placing the background and centering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from 'react';
+import Background from './components/background';
+import './style.css';
+import GlobalStyle from './style/global';
 
 function App() {
   return (
     <div className="App">
-
+      <GlobalStyle />
+      <Background />
     </div>
   );
 }
---
+++ src/components/background/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import background from '../../assets/images/background.gif'
+export default function Background(){
+  
+  return(
+    <div>
+      <img src={background} alt="Fundo de tela do jogo" />
+    </div>
+  )
+}
